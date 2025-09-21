89052ec9fbf4e4b5ca52272ca4d8ee1d4ff32a83
Pradise2 <165901189+Pradise2@users.noreply.github.com>
2025-09-21 00:07:05 +0000

    farcaster

--- src/main.tsx
+++ src/main.tsx
@@ -2,15 +2,33 @@ import { createRoot } from "react-dom/client";
 import { WagmiProvider } from "wagmi";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { config } from "@/lib/wagmi";
+import { StrictMode, useEffect } from 'react';
 import App from "./App.tsx";
 import "./index.css";
+import FrameSDK from '@farcaster/frame-sdk';
+
+function FarcasterFrameProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+  const init = async () => {
+      FrameSDK.actions.ready();
+    };
+
+    init();
+  }, []);
+
+  return <>{children}</>;
+ }
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <WagmiProvider config={config}>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
-  </WagmiProvider>,
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+      <FarcasterFrameProvider>
+          <App />
+    </FarcasterFrameProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  </StrictMode>
 );
